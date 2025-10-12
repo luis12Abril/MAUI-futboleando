c47218968e80e75b65d3f134264f26fd9cc4c2e8
Luis Barreras <luisarturobarreras@gmail.com>
2025-10-12 00:10:30 -0700

    Agregar archivos de proyecto.

---
+++ futboleando/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using futboleando.Pages;
+
+namespace futboleando
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+    }
+}
---
+++ futboleando/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace futboleando
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
