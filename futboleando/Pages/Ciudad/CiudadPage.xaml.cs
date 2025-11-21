82fa55ad11f115c0cfd432af43b42d34a200c140
Luis12Abril <luisarturobarreras@gmail.com>
2025-11-21 14:51:05 -0700

    HASTA EL VIDEO 50, FILTRO SENSITIVO

--- futboleando/Pages/Ciudad/CiudadPage.xaml.cs
+++ futboleando/Pages/Ciudad/CiudadPage.xaml.cs
@@ -8,9 +8,19 @@ namespace futboleando.Pages.Ciudad;
 public partial class CiudadPage : ContentPage
 {
 	private readonly CiudadService ciudadService;
-	//public ObservableCollection<CiudadCLS> listaCiudad { get; set; }
+	public ObservableCollection<CiudadListCLS> listaCiudad { get; set; }
+	public string nombreciudadbuscar { get; set; }
     public CiudadPage(CiudadService _ciudadService)
 	{
-		InitializeComponent();
+		InitializeComponent(); 
+		ciudadService = _ciudadService;
+		listaCiudad = ciudadService.listarciudad();
+		BindingContext = this;
 	}
+
+    private void entrynombreciudad_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		// DisplayAlert("Texto cambiado", e.NewTextValue, "OK");
+        DisplayAlert("Texto cambiado", nombreciudadbuscar, "OK");
+    }
 }
