7399c8b538dc2ca9dced1769558181115b334aec
Luis12Abril <luisarturobarreras@gmail.com>
2025-12-03 13:58:09 -0700

    HASTA EL VIDEO 58, SE RREALIZO UN FILTRO SENSITIVO

--- futboleando/Pages/Ciudad/CiudadPage.xaml.cs
+++ futboleando/Pages/Ciudad/CiudadPage.xaml.cs
@@ -9,18 +9,38 @@ public partial class CiudadPage : ContentPage
 {
 	private readonly CiudadService ciudadService;
 	public ObservableCollection<CiudadListCLS> listaCiudad { get; set; }
-	public string nombreciudadbuscar { get; set; }
+	public ObservableCollection<CiudadListCLS> listaFiltro;
+    public string nombreciudadbuscar { get; set; }
     public CiudadPage(CiudadService _ciudadService)
 	{
 		InitializeComponent(); 
 		ciudadService = _ciudadService;
 		listaCiudad = ciudadService.listarciudad();
-		BindingContext = this;
+		listaFiltro = new ObservableCollection<CiudadListCLS>(listaCiudad);
+        BindingContext = this;
 	}
 
-    private void entrynombreciudad_TextChanged(object sender, TextChangedEventArgs e)
-    {
+	private void entrynombreciudad_TextChanged(object sender, TextChangedEventArgs e)
+	{
 		// DisplayAlert("Texto cambiado", e.NewTextValue, "OK");
-        DisplayAlert("Texto cambiado", nombreciudadbuscar, "OK");
+		//DisplayAlert("Texto cambiado", nombreciudadbuscar, "OK");
+
+		listaCiudad.Clear();
+		ObservableCollection<CiudadListCLS> listaop;
+        if (string.IsNullOrEmpty(nombreciudadbuscar))
+		{
+			listaop = listaFiltro;
+			
+		}
+		else
+		{
+			var listaCiudadFiltrada = listaFiltro.Where(c => c.nombreciudad!.ToUpper().Contains(nombreciudadbuscar.ToUpper())).ToList();
+			listaop = new ObservableCollection<CiudadListCLS>(listaCiudadFiltrada);
+		}
+
+        foreach (var item in listaop)
+        {
+            listaCiudad.Add(item);
+        }
     }
 }
